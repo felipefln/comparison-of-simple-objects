eec6f5d15f79cf7a664be9e2ca71a524e15d1112
felipefln <felipecarvalho_22_@hotmail.com>
2019-02-19 19:26:38 -0300

    comparation object two show forms incorrets operator comparation == or ===

--- objects/comparation.js
+++ objects/comparation.js
@@ -16,4 +16,10 @@ let equal = true;
 for (let key in car) {
     if(car[key] !== car1[key]) equal = false;
 }
-console.log(equal);
+
+for (let key in car1) {
+    if(car1[key] !== car[key]) equal = false;
+}
+console.log(equal); 
+console.log(car == car1); // comparation false
+console.log(car === car1) // comparation false
